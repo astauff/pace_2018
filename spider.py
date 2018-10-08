2fed7d2afa8672d09dbbaf6cf39728767799f287
astauff <41757124+astauff@users.noreply.github.com>
2018-10-08 17:02:35 -0400

    Add files via upload

--- spider.py
+++ spider.py
@@ -1,20 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
-def web(page,WebUrl):
+
+#This is the function to crawl ClinGin
+
+def clingin(page,WebUrl):
+    global info
     if(page>0):
         url = WebUrl
         code = requests.get(url)
         plain = code.text
         s = BeautifulSoup(plain, "html.parser")
         for link in s.findAll(id="gene_info"):
+            stuff = link.text.split(': ')
+            for st in stuff:
+                print(st)
+
+        for link in s.findAll('a', attrs={'title':'Go to Entrez Gene'}):
             link.get('href')
-            print(link.text)
-            #if(link.text == 'Entrez Gene'):
-                #web(1, )
+            print(link.text + '\n')
+
+
+
+def clingin_2(WebUrl2):
+    url = WebUrl2
+    code = requests.get(url)
+    plain = code.text
+    s = BeautifulSoup(plain, "html.parser")
+    for link in s.findAll():
 
 
 
 
-web(1,'https://www.ncbi.nlm.nih.gov/projects/dbvar/clingen/clingen_gene.cgi?sym=cftr&subject')
 
-#'https://en.wikipedia.org/wiki/List_of_libraries'
+#clingin(1,'https://www.ncbi.nlm.nih.gov/projects/dbvar/clingen/clingen_gene.cgi?sym=cftr&subject')
+
+#clingin(1,'https://www.ncbi.nlm.nih.gov/projects/dbvar/clingen/clingen_gene.cgi?sym=brca1&subject')
