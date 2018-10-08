685fc9bcb9a91b114fd3ea7867e010a78e6893cc
astauff <41757124+astauff@users.noreply.github.com>
2018-10-08 17:05:53 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from spider import clingin
+
+clingin(1,'https://www.ncbi.nlm.nih.gov/projects/dbvar/clingen/clingen_gene.cgi?sym=cftr&subject')
+#clingin(1,'https://www.ncbi.nlm.nih.gov/projects/dbvar/clingen/clingen_gene.cgi?sym=brca1&subject')
