9fc128128fe993c2605fbf1cfb19be65af81670e
astauff <41757124+astauff@users.noreply.github.com>
2019-02-11 12:44:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from spider import clingin
+from spider import *
 
-clingin(1,'https://www.ncbi.nlm.nih.gov/projects/dbvar/clingen/clingen_gene.cgi?sym=cftr&subject')
-#clingin(1,'https://www.ncbi.nlm.nih.gov/projects/dbvar/clingen/clingen_gene.cgi?sym=brca1&subject')
+#clingen_test("BRCA1",'https://search.clinicalgenome.org/kb/genes/HGNC:1100')
+#clingen_test("CFTR",'https://search.clinicalgenome.org/kb/genes/HGNC:1884')
+
+#clingin_1(1,'https://www.ncbi.nlm.nih.gov/projects/dbvar/clingen/clingen_gene.cgi?sym=cftr&subject')
+#clingen_1("BRCA1", 1,'https://www.ncbi.nlm.nih.gov/projects/dbvar/clingen/clingen_gene.cgi?sym=brca1&subject')
+#clingin_1(1, 'https://www.ncbi.nlm.nih.gov/projects/dbvar/clingen/clingen_gene.cgi?sym=brca2&subject')
+
+#clingen_2("BRCA1",1, 'https://search.clinicalgenome.org/kb/gene-validity/9972')
+
+#clingen_3("BRCA1",1, 'https://actionability.clinicalgenome.org/ac-adult/ui/stg2SummaryRpt?doc=AC133')
+
+#send_data()
+
+list_trial('https://actionability.clinicalgenome.org/ac-adult/ui/stg2SummaryRpt?doc=AC133')
+#clingen_start('https://search.clinicalgenome.org/kb/genes/HGNC:1884')
