bcb672658ef7dc94c02d19c34c84f8ca9be39014
astauff <41757124+astauff@users.noreply.github.com>
2019-02-11 12:42:11 -0500

    Update send_data.py

--- send_data.py
+++ send_data.py
@@ -1,3 +1,10 @@
-import sqlite3
+#***** THIS IS A TEST OF SENDING DATA TO TEXT FILE *****#
 
-db = sqlite3.connect('database.db')
+def send_data():
+    file = "trial.txt"
+    open(file, "w+")
+
+    f = open(file, "a+")
+    for i in range(2):
+        f.write("Appended line %d\r\n" % (i + 1))
+    f.close()
