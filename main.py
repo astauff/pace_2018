9c2a495a92c9af75cd359746f08b2011da09d29f
astauff <41757124+astauff@users.noreply.github.com>
2019-03-08 14:47:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from spider import *
+from clingen import *
+from clingen3 import *
 
 #clingen_test("BRCA1",'https://search.clinicalgenome.org/kb/genes/HGNC:1100')
 #clingen_test("CFTR",'https://search.clinicalgenome.org/kb/genes/HGNC:1884')
@@ -10,8 +11,15 @@ from spider import *
 #clingen_2("BRCA1",1, 'https://search.clinicalgenome.org/kb/gene-validity/9972')
 
 #clingen_3("BRCA1",1, 'https://actionability.clinicalgenome.org/ac-adult/ui/stg2SummaryRpt?doc=AC133')
+#clingen_3_1("MLH1",1, 'https://actionability.clinicalgenome.org/ac-adult/ui/stg2SummaryRpt?doc=AC069')
 
-#send_data()
 
-list_trial('https://actionability.clinicalgenome.org/ac-adult/ui/stg2SummaryRpt?doc=AC133')
+
 #clingen_start('https://search.clinicalgenome.org/kb/genes/HGNC:1884')
+
+#ref_sort('https://actionability.clinicalgenome.org/ac-adult/ui/stg2SummaryRpt?doc=AC133')
+
+
+#clingen_3_1("BRCA1",1, 'https://actionability.clinicalgenome.org/ac-adult/ui/stg2SummaryRpt?doc=AC133')
+
+kid_count("BRCA1", 'https://actionability.clinicalgenome.org/ac-adult/ui/stg2SummaryRpt?doc=AC133')
